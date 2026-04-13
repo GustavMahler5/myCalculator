87fb14320ee5a426b76c6df976534e796331214f
Godot107 <jasonholtman15@gmail.com>
2026-04-13 14:04:15 -0700

    Added matrix functions to header file

--- include/calculator.h
+++ include/calculator.h
@@ -14,4 +14,16 @@ class Calculator {
     float log(float x, float y) {}
     int factorial(int x) {}
 
+    void matrixPrint(float mat[3][3]) {}
+    int matrixEquals(float mat1[3][3], float mat2[3][3]) {}
+    void matrixAdd(float mat1[3][3], float mat2[3][3], float result[3][3]) {}
+    void matrixMultiply(float mat1[3][3], float mat2[3][3], float result[3][3]) {}
+    void matrixScalarAdd(float x, float mat[3][3], float result[3][3]) {}
+    void matrixScalarMultiply(float x, float mat[3][3], float result[3][3]) {}
+    float matrixTrace(float mat[3][3]) {}
+    void matrixTranspose(float mat[3][3], float result[3][3]) {}
+    void matrixSubmatrix(int x, int y, float mat[3][3], float result[2][2]) {}
+    float matrixDeterminant(float mat[3][3]) {}
+    void matrixInverse(float mat[3][3], float result[3][3]) {}
+    
 };
