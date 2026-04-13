22045ed47df243a40a8604a2a51b0092f8e7612f
Godot107 <jasonholtman15@gmail.com>
2026-04-13 13:34:51 -0700

    built basic arithmetic operations in header and cpp files

---
+++ include/calculator.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+class Calculator {
+
+    float add(float x, float y) {}
+    float subtract(float x, float y) {}
+    float multiply(float x, float y) {}
+    float divide(float x, float y) {}
+    float exponent(float x, float y) {}
+    float root(float x, float y) {}
+    float log(float x, float y) {}
+    int factorial(int x) {}
+
+};
